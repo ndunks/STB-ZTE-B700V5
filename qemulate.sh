bb67076e0ff0d44aeb3959731e528f1617e8371b
Mochamad Arifin <klampok.child@gmail.com>
2022-01-29 00:09:31 +0700

    uboot qemu virt

--- qemulate.sh
+++ qemulate.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+# nodemon -I -w qemulate.sh --delay 0.3 -x "./qemulate.sh || false"
 # Not working
 qemu-system-arm -m 256M -M versatilepb -cpu arm1176 \
-    -kernel kernel \
-    -append "console=ttyS0 rw init=/init root=/dev/sda rootfstype=jffs2 rw" \
-    -hda rootfs1.bin \
+    -hda dump/mtdblock0 \
     -nographic
-
-    # -serial stdio
+# -serial stdio
+    
+# qemu-system-arm -m 256M -M virt -nographic \
+#     -bios qemu/u-boot-nodtb.bin \
+#     -kernel kernel/kernel1 \
+#     -device nand,chip_id=0x59,id=nand0 \
+#     -drive if=mtd,format=raw,file=dump/mtdblock0,id=nand0 \
+#     -append "console=ttyS0 rw init=/init root=/dev/sda rootfstype=jffs2 rw"
